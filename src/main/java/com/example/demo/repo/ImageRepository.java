5f4a0e66c30e542d8afdcacafc709714b01c3818
Mohamed Ali Hmem <50034291+MohamedAliHmem@users.noreply.github.com>
2024-11-30 00:16:05 +0100

    commit 1

--- src/main/java/com/example/demo/repo/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entities.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
